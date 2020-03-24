9c4146c9179071b0e497394f21b1a9110e516902
SGupta101 <stutigupta101@gmail.com>
2020-03-24 13:06:16 -0400

    Initial commit

---
+++ app/src/main/java/com/guptastuti/finallab04viewdemonstration/MainActivity.java
@@ -0,0 +1,13 @@
+package com.guptastuti.finallab04viewdemonstration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraintlayout_7);
+    }
+}
